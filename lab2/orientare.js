68bb9fd468f7d6079042816bc54b5b45e04a8929
mizuioshi <108391207+mizuioshi@users.noreply.github.com>
2022-12-19 19:26:54 +0200

    1

--- lab2/orientare.js
+++ lab2/orientare.js
@@ -1,15 +1,15 @@
-window.addEventListener("deviceorientation", on_device_orientation);
+window.addEventListener('deviceorientation', on_device_orientation);
 function on_device_orientation(evt) {
   let alpha = evt.alpha;
   let beta = evt.beta;
   let gamma = evt.gamma;
 
-  document.getElementById("a").innerHTML = "alpha = " + alpha;
-  document.getElementById("b").innerHTML = "beta = " + beta;
-  document.getElementById("c").innerHTML = "gamma = " + gamma;
+  document.getElementById('a').innerHTML = 'alpha = ' + alpha;
+  document.getElementById('b').innerHTML = 'beta = ' + beta;
+  document.getElementById('c').innerHTML = 'gamma = ' + gamma;
 
-  const canvas = document.getElementById("canvas");
-  let context = canvas.getContext("2d");
+  const canvas = document.getElementById('canvas');
+  let context = canvas.getContext('2d');
   let centru = { x: canvas.width / 2, y: canvas.height / 2 };
   let latura_patrat = 50;
   context.setTransform(1, 0, 0, 1, 0, 0);
