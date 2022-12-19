b18d33b26aff5219d94fc838cdc8dd7e166c3b13
mizuioshi <108391207+mizuioshi@users.noreply.github.com>
2022-12-19 19:27:28 +0200

    2

--- lab2/beta.js
+++ lab2/beta.js
@@ -4,14 +4,14 @@ function on_device_orientation(evt) {
   let beta = evt.beta;
   let gamma = evt.gamma;
 
-  document.getElementById("a").innerHTML = "alpha = " + alpha;
-  document.getElementById("b").innerHTML = "beta = " + beta;
-  document.getElementById("c").innerHTML = "gamma = " + gamma;
+  document.getElementById('a').innerHTML = 'alpha = ' + alpha;
+  document.getElementById('b').innerHTML = 'beta = ' + beta;
+  document.getElementById('c').innerHTML = 'gamma = ' + gamma;
 
-  const canvas = document.getElementById("canvas");
+  const canvas = document.getElementById('canvas');
   let context = canvas.getContext("2d");
   context.clearRect(0, 0, canvas.width, canvas.height);
-  context.fillStyle = "#6A0888";
+  context.fillStyle = '#6A0888';
   context.beginPath();
   context.fillRect(180, 150, evt.beta, 50);
 }
