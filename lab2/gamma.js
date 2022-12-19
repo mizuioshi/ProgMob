d1fc77162e4b75213d2edb8889b9afa2bc959b3e
mizuioshi <108391207+mizuioshi@users.noreply.github.com>
2022-12-19 19:27:48 +0200

    3

--- lab2/gamma.js
+++ lab2/gamma.js
@@ -1,17 +1,17 @@
-window.addEventListener("deviceorientation", on_device_orientation);
+window.addEventListener('deviceorientation', on_device_orientation);
 function on_device_orientation(evt) {
   let alpha = evt.alpha;
   let beta = evt.beta;
   let gamma = evt.gamma;
 
-  document.getElementById("a").innerHTML = "alpha = " + alpha;
-  document.getElementById("b").innerHTML = "beta = " + beta;
-  document.getElementById("c").innerHTML = "gamma = " + gamma;
+  document.getElementById('a').innerHTML = 'alpha = ' + alpha;
+  document.getElementById('b').innerHTML = 'beta = ' + beta;
+  document.getElementById('c').innerHTML = 'gamma = ' + gamma;
 
-  const canvas = document.getElementById("canvas");
-  let context = canvas.getContext("2d");
+  const canvas = document.getElementById('canvas');
+  let context = canvas.getContext('2d');
   context.clearRect(0, 0, canvas.width, canvas.height);
-  context.fillStyle = "rgb(0,128,128)";
+  context.fillStyle = 'rgb(0,128,128)';
   context.beginPath();
   context.fillRect(180, 150, evt.gamma, 50);
 }
